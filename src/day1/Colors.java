4f06a18f08c433a6b226e047d8c6f60c2724d3eb
github alison9797 <aliszak9727@gmail.com>
2020-05-03 10:40:30 -0400

    Added bew color

--- src/day1/Colors.java
+++ src/day1/Colors.java
@@ -3,7 +3,9 @@ package day1;
 public class Colors {
     public static void main(String[] args) {
 
-        String[] colors = {"red", "blue", "green"};
+        String[] colors = {"red", "blue", "green", "yellow"};
+
+        System.out.println("My colors");
 
     }
 }
